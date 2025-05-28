3b1b1ecf1ce72e9ea2ca321ebc573c10973613aa
Bertogim <biosgim@gmail.com>
2025-05-28 21:40:04 +0200

    Update

--- auto-quality.js
+++ auto-quality.js
@@ -1,8 +1,6 @@
 /*
 Current version: v0.1.1
 Download the auto-quality.js script and add it to your bluemap server
-Or add it to your bluemap html to always have the latest version:
-Add to the header in the index.html: `<script src="https://raw.githubusercontent.com/Bertogim/bluemap-auto-quality/refs/heads/main/auto-quality.js"></script>`
 */
 
 
