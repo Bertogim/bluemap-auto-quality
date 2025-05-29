3af60658063b2c003ae105ef5fdba0c063f8c365
Bertogim <biosgim@gmail.com>
2025-05-29 21:37:21 +0200

    v0.1.3

--- auto-quality-beta.js
+++ auto-quality-beta.js
@@ -33,38 +33,6 @@ Download the auto-quality.js script and add it to your bluemap server
     let autoHiresEnabled = localStorage.getItem("autoHiresEnabled") !== "false";     // default true
     let autoLowresEnabled = localStorage.getItem("autoLowresEnabled") !== "false";   // default true
 
-
-    function loadStatsJs(callback) {
-        if (window.Stats) return callback();
-
-        const script = document.createElement("script");
-        script.innerHTML =
-            `// stats.js - http://github.com/mrdoob/stats.js
-            (function (f, e) { "object" === typeof exports && "undefined" !== typeof module ? module.exports = e() : "function" === typeof define && define.amd ? define(e) : f.Stats = e() })(this, function () {
-                var f = function () {
-                    function e(a) { c.appendChild(a.dom); return a } function u(a) { for (var d = 0; d < c.children.length; d++)c.children[d].style.display = d === a ? "block" : "none"; l = a } var l = 0, c = document.createElement("div"); c.style.cssText = "position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000"; c.addEventListener("click", function (a) {
-                        a.preventDefault();
-                        u(++l % c.children.length)
-                    }, !1); var k = (performance || Date).now(), g = k, a = 0, r = e(new f.Panel("FPS", "#0ff", "#002")), h = e(new f.Panel("MS", "#0f0", "#020")); if (self.performance && self.performance.memory) var t = e(new f.Panel("MB", "#f08", "#201")); u(0); return {
-                        REVISION: 16, dom: c, addPanel: e, showPanel: u, begin: function () { k = (performance || Date).now() }, end: function () {
-                            a++; var c = (performance || Date).now(); h.update(c - k, 200); if (c > g + 1E3 && (r.update(1E3 * a / (c - g), 100), g = c, a = 0, t)) {
-                                var d = performance.memory; t.update(d.usedJSHeapSize /
-                                    1048576, d.jsHeapSizeLimit / 1048576)
-                            } return c
-                        }, update: function () { k = this.end() }, domElement: c, setMode: u
-                    }
-                }; f.Panel = function (e, f, l) {
-                    var c = Infinity, k = 0, g = Math.round, a = g(window.devicePixelRatio || 1), r = 80 * a, h = 48 * a, t = 3 * a, v = 2 * a, d = 3 * a, m = 15 * a, n = 74 * a, p = 30 * a, q = document.createElement("canvas"); q.width = r; q.height = h; q.style.cssText = "width:80px;height:48px"; var b = q.getContext("2d"); b.font = "bold " + 9 * a + "px Helvetica,Arial,sans-serif"; b.textBaseline = "top"; b.fillStyle = l; b.fillRect(0, 0, r, h); b.fillStyle = f; b.fillText(e, t, v);
-                    b.fillRect(d, m, n, p); b.fillStyle = l; b.globalAlpha = .9; b.fillRect(d, m, n, p); return { dom: q, update: function (h, w) { c = Math.min(c, h); k = Math.max(k, h); b.fillStyle = l; b.globalAlpha = 1; b.fillRect(0, 0, r, m); b.fillStyle = f; b.fillText(g(h) + " " + e + " (" + g(c) + "-" + g(k) + ")", t, v); b.drawImage(q, d + a, m, n - a, p, d, m, n - a, p); b.fillRect(d + n - a, m, a, p); b.fillStyle = l; b.globalAlpha = .9; b.fillRect(d + n - a, m, a, g((1 - h / w) * p)) } }
-                }; return f
-            }); `;
-        script.onload = () => {
-            if (window.Stats) callback();
-            else console.error("Stats failed to load.");
-        };
-        document.head.appendChild(script);
-    }
-
     function createAutoButtons(qualityGroup, hiresGroup, lowresGroup) {
         if (!qualityGroup || !hiresGroup || !lowresGroup) return false;
 
@@ -109,11 +77,6 @@ Download the auto-quality.js script and add it to your bluemap server
     }
 
     function setupAutoQualityControl() {
-        const stats = new Stats();
-        stats.showPanel(0);
-        stats.dom.style.display = "none";
-        document.body.appendChild(stats.dom);
-
         function safeSetData(prop, val) {
             if (bluemap.mapViewer.data[prop] !== val) {
                 bluemap.mapViewer.data[prop] = val;
@@ -263,11 +226,9 @@ Download the auto-quality.js script and add it to your bluemap server
         }
 
         function startAnimationLoop() {
-            if (!isAnimating && document.visibilityState === "visible") {
                 lastTime = performance.now();
                 frameCount = 0;
                 animationFrameId = requestAnimationFrame(animate);
-            }
         }
 
         function stopAnimationLoop() {
@@ -337,7 +298,7 @@ Download the auto-quality.js script and add it to your bluemap server
             typeof bluemap.mapViewer.data.loadedLowresViewDistance === "number"
         ) {
             clearInterval(waitForBlueMap);
-            loadStatsJs(setupAutoQualityControl);
+            setupAutoQualityControl();
         }
     }, 200);
 })();
